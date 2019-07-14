f81fc8fbb7d9630ac1a0597f0d0d1ae2aefa406e
Jedidiah Broadbent <hello@jedidiah.dev>
2019-07-14 22:35:09 +0100

    Add prettier precommit hook

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,22 +1,23 @@
-const axios = require('axios');
-const crypto = require('crypto');
-
+const axios = require("axios");
+const crypto = require("crypto");
 
 const internalType = type => `soundcloud${type}`;
 
 const internalId = (id, type) => `${internalType(type)}-${id}`;
 
-const createHash = (obj) => crypto.createHash('md5')
-                                  .update(JSON.stringify(obj))
-                                  .digest('hex');
+const createHash = obj =>
+  crypto
+    .createHash("md5")
+    .update(JSON.stringify(obj))
+    .digest("hex");
 
-const processEntity = (entity) => ({
+const processEntity = entity => ({
   ...entity,
-  parent: '__SOURCE__',
+  parent: "__SOURCE__",
   children: [],
   internal: {
     type: internalType(entity.kind),
-    contentDigest: createHash(entity),
+    contentDigest: createHash(entity)
   }
 });
 
@@ -25,7 +26,8 @@ const collectTracksFromPlaylist = (accTracks, playlist) => [
   ...playlist.tracks
 ];
 
-const addIfUnique = (uniqueItems, item) => (uniqueItems.find(t => t.id === item.id))
+const addIfUnique = (uniqueItems, item) =>
+  uniqueItems.find(t => t.id === item.id)
     ? uniqueItems
     : [...uniqueItems, item];
 
@@ -42,42 +44,48 @@ function fetchUserResource(resource, userID, clientID) {
 }
 
 function linkNodes(nodes) {
-  return nodes.map((n) => {
-    n.soundcloud_id = n.id;
-    n.id = internalId(n.id, n.kind);
-    return n;
-  }).map((n) => {
-    let user = undefined;
+  return nodes
+    .map(n => {
+      n.soundcloud_id = n.id;
+      n.id = internalId(n.id, n.kind);
+      return n;
+    })
+    .map(n => {
+      let user = undefined;
 
-    if (n.user) {
-      user = nodes.find(u => u.id === internalId(n.user.id, n.user.kind));
+      if (n.user) {
+        user = nodes.find(u => u.id === internalId(n.user.id, n.user.kind));
 
-      if (user) {
-        // Add link to user node
-        n.user___NODE = internalId(n.user.id, n.user.kind);
-        delete n.user;
+        if (user) {
+          // Add link to user node
+          n.user___NODE = internalId(n.user.id, n.user.kind);
+          delete n.user;
 
-        // Add reverse link to node on user
-        addReverseLink(n, user);
+          // Add reverse link to node on user
+          addReverseLink(n, user);
+        }
       }
-    }
 
-    if (n.tracks) {
-      // Add links to tracks on node
-      n.tracks___NODE = n.tracks.map(t => internalId(t.id, t.kind));
+      if (n.tracks) {
+        // Add links to tracks on node
+        n.tracks___NODE = n.tracks.map(t => internalId(t.id, t.kind));
 
-      // Add reverse links to node on tracks and users
-      n.tracks.map((t) => {
-        const track = nodes.find(u => (u.id === internalId(t.id, t.kind)));
-        if (track) { addReverseLink(n, track); }
-        if (user) { addReverseLink(n, user); }
-      });
+        // Add reverse links to node on tracks and users
+        n.tracks.map(t => {
+          const track = nodes.find(u => u.id === internalId(t.id, t.kind));
+          if (track) {
+            addReverseLink(n, track);
+          }
+          if (user) {
+            addReverseLink(n, user);
+          }
+        });
 
-      delete n.tracks;
-    }
+        delete n.tracks;
+      }
 
-    return n;
-  });
+      return n;
+    });
 }
 
 exports.sourceNodes = async ({ boundActionCreators }, { userID, clientID }) => {
@@ -85,27 +93,32 @@ exports.sourceNodes = async ({ boundActionCreators }, { userID, clientID }) => {
 
   try {
     // Fetch data
-    const userInfo = await fetchUserResource('', userID, clientID);
-    const playlists = await fetchUserResource('/playlists', userID, clientID);
-    const tracks = await fetchUserResource('/tracks', userID, clientID);
+    const userInfo = await fetchUserResource("", userID, clientID);
+    const playlists = await fetchUserResource("/playlists", userID, clientID);
+    const tracks = await fetchUserResource("/tracks", userID, clientID);
 
-
-    const tracksFromPlaylists = playlists.data.reduce(collectTracksFromPlaylist, []);
+    const tracksFromPlaylists = playlists.data.reduce(
+      collectTracksFromPlaylist,
+      []
+    );
     const combinedTracks = tracksFromPlaylists.reduce(addIfUnique, tracks.data);
 
     const users = [userInfo.data];
     const combinedUsers = combinedTracks.reduce(addIfUnique, users);
 
-    const entities = linkNodes([...users, ...playlists.data, ...combinedTracks]);
+    const entities = linkNodes([
+      ...users,
+      ...playlists.data,
+      ...combinedTracks
+    ]);
 
     // Process data into nodes.
     entities.forEach(entity => createNode(processEntity(entity)));
 
     // We're done, return.
     return;
-
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
-}
+};
